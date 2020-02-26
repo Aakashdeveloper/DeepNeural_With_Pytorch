3d3a6309813bf6c83b01870796f8cbb94daf0bd5
aakashDeveloper <ahanda205@gmail.com>
2020-02-26 07:38:30 +0000

    a

---
+++ pytorch_image.py
@@ -0,0 +1,35 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+        transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]
+)
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                    download=True, transform=transform)
+    
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+testloader = torch.utils.data.DataLoader(trainset, batch_size=4,shuffle=False, num_workers=2)
+
+classes = ('plane','car','bird','cat','deer','dog','frog','horse','ship','truck')
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def imshow(img):
+    img = img / 2 +0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg,(1, 2, 0)))
+
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+imshow(torchvision.utils.make_grid(images))
+print(' '.join('%5s' % classes[labebls[j]] for j in range(4)))
